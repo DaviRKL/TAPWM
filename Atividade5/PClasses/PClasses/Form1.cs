850a162d35f91db803415b8a0b5e3853b4268fb8
Davi Konuma <95696184+DaviRKL@users.noreply.github.com>
2024-03-27 16:45:00 -0300

    Add files via upload

---
+++ Atividade5/PClasses/PClasses/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PClasses
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FrnMensalista frnMensalista = new FrnMensalista();
+            frnMensalista.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FrnHorista frnHorista = new FrnHorista();
+            frnHorista.Show();
+        }
+    }
+}
---
+++ Atividade5/PClasses/PClasses/Horista.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PClasses
+{
+    internal class Horista: Empregado
+    {
+        public double SalarioHora {  get; set; }
+        public double NumeroHora { get; set; }
+        public int DiasFalta { get; set; }
+
+        public override int TempoTrabalho()
+        {
+            TimeSpan span =
+                DateTime.Today.Subtract(DataEntradaEmpresa);
+            return (Convert.ToInt32(span.Days) - DiasFalta);
+        }
+        public override double SalarioBruto()
+        {
+            return SalarioHora * NumeroHora;
+        }
+
+        public double SalarioBruto(double aumento)
+        {
+            double sal = SalarioHora * NumeroHora;
+            return (sal + sal * (aumento / 100));
+        }
+    }
+
+    
+}
