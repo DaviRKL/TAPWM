042865630e09b4c8a3bd3ec9012aa77d28a418ea
Davi Konuma <95696184+DaviRKL@users.noreply.github.com>
2024-03-28 10:41:58 -0300

    Add files via upload

---
+++ Atividade 6/Media.js
@@ -0,0 +1,6 @@
+var nome = prompt("Insira seu nome");
+var nota1 = parseFloat(prompt("Insira a nota 1"));
+var nota2 = parseFloat(prompt("Insira a nota 2"));
+var nota3 = parseFloat(prompt("Insira a nota 3"));
+var media = nota1 + nota2 + nota3 / 3;
+alert(nome +" a sua nota é" + media);
---
+++ Atividade 6/Operacoes.js
@@ -0,0 +1,7 @@
+var numero1 = parseFloat(prompt("Insira o primeiro número"));
+var numero2= parseFloat(prompt("Insira o segundo número"));
+var soma = numero1  + numero2;
+var sub = numero1 - numero2;
+var mult = numero1 * numero2;
+var divi = numero1 / numero2
+alert("A soma é: " + soma + " A substração é: " + sub + " A multiplicação é: " + mult +" A divisão é: " + divi);
